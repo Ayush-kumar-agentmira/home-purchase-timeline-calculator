ac2655b558da21dddbe3a8ea20b4e36ea4189e23
Ayush-kumar-agentmira <ayush.kumar@agentmira.ai>
2025-04-30 15:55:15 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import HomePurchase from './HomePurchase'
-import HomePurchase1 from './HomePurchase1'
-import HomePurchaseComparison from './HomePurchaseComparison'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <HomePurchase />
-      {/* <HomePurchase1 /> */}
-      {/* <HomePurchaseComparison /> */}
+      
         
     </>
   )
